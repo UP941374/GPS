66cbe0d19c80fc99f0817d38af997daf5b353d0a
UP941374 <up941374@myport.ac.uk>
2021-03-02 20:00:37 +0000

    ukraine fix

--- ukraine.js
+++ ukraine.js
@@ -33,7 +33,7 @@ let files = [
 function tostr(x){
   let output = 'https://gpx",studio.github.io/?state=%7B%22urls%22:%5B'
   for (let file of x) {
-    output = output + "%22https%3A%2F%2Fraw.githubusercontent.com%2FUP941374%2FGPS%2Fmain%2Fgpx",%2FUKRAINE"2019%2F" + file + "%22,"
+    output = output + "%22https%3A%2F%2Fraw.githubusercontent.com%2FUP941374%2FGPS%2Fmain%2FGPX%2FUKRAINE2019%2F" + file + "%22,"
   }
   mapsrc = output.slice(0, -1);
   mapsrc = mapsrc + '%5D%7D&embed'
