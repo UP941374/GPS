706cedabbd33303f345d68e5569a73d900688cf0
Arek Uchymiak <auser93@gmail.com>
2024-07-03 11:07:04 +0100

    globetrotter created

---
+++ globetroter.js
@@ -0,0 +1,18 @@
+let map = document.querySelector("#map");
+
+let files = [
+  "UKRAINE2019.gpx"
+];
+
+
+function tostr(x){
+  let output = 'https://gpxstudio.github.io/?state=%7B%22urls%22:%5B'
+  for (let file of x) {
+    output = output + "%22https%3A%2F%2Fraw.githubusercontent.com%2FUP941374%2FGPS%2Fmain%2Fmerged%2F" + file + "%22,"
+  }
+  mapsrc = output.slice(0, -1);
+  mapsrc = mapsrc + '%5D%7D&embed'
+  return mapsrc;
+}
+
+map.src = tostr(files);
